11c82f41c9961b824fdca5928696dd63dc908bd8
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-13 21:19:29 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+let text = document.querySelector('input')
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  addTodo(e.target['new-task-description'].value)
+  form.reset()
+})
 });
+
+function addTodo(updatelist){
+let li = document.createElement('li')
+let btn = document.createElement('button')
+btn.textContent = 'delete'
+btn.addEventListener('click', deleteList)
+li.textContent = `${updatelist}  `
+document.querySelector('#list').appendChild(li)
+li.appendChild(btn)
+
+}
+
+function deleteList(e){
+e.target.parentNode.remove()
+}
+
